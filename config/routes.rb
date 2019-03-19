8d415d318c5b3b7af1c497de1c555e82286551a7
Andrey Spies <andreyspies@gmail.com>
2019-03-19 15:06:45 -0300

    Added routes and instance of new Contact to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'contacts/new'
+  get 'contacts/create'
   devise_for :users
+  resources :contacts, only: %i[new create]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
