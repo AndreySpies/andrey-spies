41dc36fee0d11f7f60ed8a8b1d203b3f41ac0b95
Andrey Spies <andreyspies@gmail.com>
2019-03-19 19:27:55 -0300

    Added postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,16 +4,21 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Email Settings
-  config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    user_name: ENV['GMAIL_USERNAME'],
-    password: ENV['GMAIL_PASSWORD'],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.delivery_method     = :postmark
+  config.action_mailer.postmark_settings   = { api_key: ENV['POSTMARK_API_KEY'] }
+  config.action_mailer.default_url_options = { host: "https://www.andreyspies.dev" }
+
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #  address: 'smtp.gmail.com',
+  #  port: 587,
+  #  user_name: ENV['GMAIL_USERNAME'],
+  #  password: ENV['GMAIL_PASSWORD'],
+  #  authentication: 'plain',
+  #  enable_starttls_auto: true
+  # }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
